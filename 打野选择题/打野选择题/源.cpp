9e78169706753f17b2e61003ebaaea0d32e43953
Mrzhailiming <401875751@qq.com>
2020-02-04 13:33:32 +0800

    day2/1

---
+++ 打野选择题/打野选择题/源.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+
+//day2/1
+//class Solution {
+//public:
+//	int removeElement(vector<int>& nums, int val) {
+//		int len = nums.size();
+//		int pos = 0;
+//		while (pos < len) {
+//			if (nums[pos] == val)
+//				nums[pos] = nums[--len];
+//			else
+//				++pos;
+//		}
+//
+//		return len;
+//	}
+//};
+
+
+
+
+
+
+int main(){
+
+	
+	return 0;
+}
