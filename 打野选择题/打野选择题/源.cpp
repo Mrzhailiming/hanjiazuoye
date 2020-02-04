2e7c3faa33fb76a5bac2de28ccdfed341e491c05
Mrzhailiming <401875751@qq.com>
2020-02-04 15:25:30 +0800

    day2/2

--- 打野选择题/打野选择题/源.cpp
+++ 打野选择题/打野选择题/源.cpp
@@ -21,8 +21,24 @@ using namespace std;
 //};
 
 
-
-
+//day2/2
+//int searchInsert(vector<int>& nums, int target) {
+//	int len = nums.size();
+//	int i = 0;
+//	while (i < len){
+//		if (nums[i] == target){
+//			return i;
+//		}
+//		if (nums[0] > target){
+//			return 0;
+//		}
+//		if (nums[i] > target && nums[i - 1] < target){
+//			return i;
+//		}
+//		++i;
+//	}
+//	return len;
+//}
 
 
 int main(){
