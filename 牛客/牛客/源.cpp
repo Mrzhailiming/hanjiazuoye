6303e64f3ba1ab7bb6cba6116dd1c0931846479d
Mrzhailiming <401875751@qq.com>
2020-03-01 16:09:30 +0800

    数祖第k大的元素

--- 牛客/牛客/源.cpp
+++ 牛客/牛客/源.cpp
@@ -1,15 +1,31 @@
 #include <iostream> 
 using namespace std;
-int a[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
-void fun(int *pa, int n);
-int main()
-{
-	int m = 10;
-	fun(a, m);
-	cout << a[6] << endl;
-}
-void fun(int *pa, int n)
-{
-	for (int i = n - 1; i > 0; i--)
-		*(pa + 6) += pa[i];
-}
+//int a[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+//void fun(int *pa, int n);
+//int main()
+//{
+//	int m = 10;
+//	fun(a, m);
+//	cout << a[6] << endl;
+//}
+//void fun(int *pa, int n)
+//{
+//	for (int i = n - 1; i > 0; i--)
+//		*(pa + 6) += pa[i];
+//}
+
+//数组第k大的元素
+//class Solution {
+//public:
+//	int findKthLargest(vector<int>& nums, int k) {
+//		multiset<int> s;
+//		for (const auto& e : nums){
+//			s.insert(e);
+//		}
+//		auto it = s.end();
+//		while (k--){
+//			--it;
+//		}
+//		return *it;
+//	}
+//};
