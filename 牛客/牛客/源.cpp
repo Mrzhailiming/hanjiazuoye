1a006a51972a6514c736e2ff1315dd172ef2e938
Mrzhailiming <401875751@qq.com>
2020-03-11 21:50:13 +0800

    0311

--- 牛客/牛客/源.cpp
+++ 牛客/牛客/源.cpp
@@ -1,4 +1,4 @@
-
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <iostream>
 #include <map>
@@ -34,33 +34,78 @@ using namespace std;
 //	}
 //};
 
-#include <string>
+//#include <string>
+//
+//
+//int main(){
+//	string s;
+//	while (getline(cin, s)){
+//		s += '\0';
+//		int len = s.size();
+//		int i = 0;
+//		string tmp;
+//		map<string, int> mp;
+//		for (; i <= len; ++i){
+//			if (s[i] == ' ' || s[i] == '.'
+//				|| s[i] == '(' || s[i] == ','
+//				|| s[i] == '\0'){
+//				if (tmp != "")
+//					++mp[tmp];
+//				tmp = "";
+//			}
+//			else{
+//				tmp += tolower(s[i]);
+//			}
+//		}
+//		for (const auto& e : mp){
+//			cout << e.first << ":" << e.second << endl;
+//		}
+//	}
+//	
+//	return 0;
+//}
+
+//void GetMemory(char *p)
+//{
+//	p = (char *)malloc(100);
+//}
+//void Test(void)
+//{
+//	char *str = NULL;
+//	GetMemory(str);
+//	strcpy(str, "hello world");
+//	printf(str);
+//}
+//int main(){
+//	Test();
+//	return 0;
+//}
 
 
-int main(){
-	string s;
-	while (getline(cin, s)){
-		s += '\0';
-		int len = s.size();
-		int i = 0;
-		string tmp;
-		map<string, int> mp;
-		for (; i <= len; ++i){
-			if (s[i] == ' ' || s[i] == '.'
-				|| s[i] == '(' || s[i] == ','
-				|| s[i] == '\0'){
-				if (tmp != "")
-					++mp[tmp];
-				tmp = "";
-			}
-			else{
-				tmp += tolower(s[i]);
-			}
-		}
-		for (const auto& e : mp){
-			cout << e.first << ":" << e.second << endl;
-		}
+//二叉树的公共祖先 lc236
+bool _low(TreeNode* root, TreeNode* tar){
+	if (root == NULL){
+		return false;
 	}
-	
-	return 0;
+	if (root->val == tar->val){
+		return true;
+	}
+	return _low(root->left, tar)
+		|| _low(root->right, tar);
+}
+TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+	//根与其中一个相符合
+	if (root->val == p->val || root->val == q->val){
+		return root;
+	}
+	//存在于一边
+	//p和q存在左边
+	if (_low(root->left, p) && _low(root->left, q)){
+		return lowestCommonAncestor(root->left, p, q);
+	}
+	//p和q存在右边
+	else if (_low(root->right, p) && _low(root->right, q)){
+		return lowestCommonAncestor(root->right, p, q);
+	}
+	return root;
 }
