2f2f60da69315a0fd8b1a0ffba7df1a2c8684423
Mrzhailiming <401875751@qq.com>
2020-03-23 20:11:56 +0800

    saima

--- LeetCode/LeetCode/LeetCode/main.cpp
+++ LeetCode/LeetCode/LeetCode/main.cpp
@@ -37,19 +37,92 @@ using namespace std;
 //	}
 //};
 
-int main() {
-	int a = 0;
-	while (a--){
-		printf("%d\n", 10);
+//int main() {
+//	int a = 0;
+//	while (a--){
+//		printf("%d\n", 10);
+//	}
+//	vector<vector<int>*> v;
+//	int i = 0;
+//	const int *const p = &a;
+//	i = -1;
+//	const int ic = i;
+//	const int *pic = &ic;
+//	//int *const cpi = &ic;
+//	const int *const cpic = &ic;
+//	return 0;
+//}
+//
+//int main(){
+//	char* str = "dadas";
+//	string s(str, str + 2);
+//	vector<int> v;
+//	v.reserve(100);
+//	v.reserve(101);
+//	return 0;
+//}
+
+//#include <unordered_set>
+//#include <set>
+//#include <string>
+//#include <map>
+//
+//class data{
+//public:
+//	int a;
+//	int b;
+//};
+//
+//int main(){
+//	
+//	map<int, int> m;
+//	auto ret = m.insert(make_pair(1, 1));
+//	ret = m.insert(make_pair(1, 1));
+//	ret = m.insert(make_pair(1, 1));
+//	return 0;
+//}
+
+//int main(){
+//	int M, N;
+//	while (scanf("%d %d", &M, &N)){
+//		int count = 0;
+//		while (N--){
+//			int a, b, c;
+//			scanf("%d %d %d", &a, &b, &c);
+//			if (c == 1){
+//				++count;
+//			}
+//		}
+//		printf("%d\n", count);
+//	}
+//	return 0;
+//}
+
+#include <string>
+#include <iostream>
+int main(){
+	string a, b;
+
+	std::cin >> a >> b;
+	int totalNum = a.size();
+	int correctNum = 0;
+	for (int i = 0; i < totalNum; ++i){
+		if ((a[i] >= 'A' && a[i] <= 'Z') ||
+			(a[i] >= 'a' && a[i] <= 'z') ||
+			(a[i] >= '0' && a[i] <= '9')){
+			//对应位为1
+			if (b[i] == '1'){
+				++correctNum;
+			}
+		}
+		else{
+			//对应位为0
+			if (b[i] == '0'){
+				++correctNum;
+			}
+		}
 	}
-	vector<vector<int>*> v;
-	int i = 0;
-	const int *const p = &a;
-	int i = -1;
-	const int ic = i;
-	const int *pic = &ic;
-	//int *const cpi = &ic;
-	const int *const cpic = &ic;
+	double ret = correctNum * 100.0 / totalNum;
+	printf("%.2lf\n", ret);
 	return 0;
 }
-
