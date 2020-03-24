821d5550c819f660aea7c1c03b43c4317e723626
Mrzhailiming <401875751@qq.com>
2020-03-24 20:33:52 +0800

    .

--- LeetCode/LeetCode/LeetCode/main.cpp
+++ LeetCode/LeetCode/LeetCode/main.cpp
@@ -97,32 +97,108 @@ using namespace std;
 //	}
 //	return 0;
 //}
+//
+//#include <string>
+//#include <iostream>
+//int main(){
+//	string a, b;
+//
+//	std::cin >> a >> b;
+//	int totalNum = a.size();
+//	int correctNum = 0;
+//	for (int i = 0; i < totalNum; ++i){
+//		if ((a[i] >= 'A' && a[i] <= 'Z') ||
+//			(a[i] >= 'a' && a[i] <= 'z') ||
+//			(a[i] >= '0' && a[i] <= '9')){
+//			//对应位为1
+//			if (b[i] == '1'){
+//				++correctNum;
+//			}
+//		}
+//		else{
+//			//对应位为0
+//			if (b[i] == '0'){
+//				++correctNum;
+//			}
+//		}
+//	}
+//	double ret = correctNum * 100.0 / totalNum;
+//	printf("%.2lf\n", ret);
+//	return 0;
+//}
 
-#include <string>
-#include <iostream>
-int main(){
-	string a, b;
 
-	std::cin >> a >> b;
-	int totalNum = a.size();
-	int correctNum = 0;
-	for (int i = 0; i < totalNum; ++i){
-		if ((a[i] >= 'A' && a[i] <= 'Z') ||
-			(a[i] >= 'a' && a[i] <= 'z') ||
-			(a[i] >= '0' && a[i] <= '9')){
-			//对应位为1
-			if (b[i] == '1'){
-				++correctNum;
+
+#include <map>
+#include <math.h>
+#define max(a,b)    (((a) > (b)) ? (a) : (b))
+int rob(vector<int>& nums) {
+	map<int, bool> mp;
+	int len = nums.size();
+	int* dp = new int[len];
+	mp[0] = true;
+	if (len == 0){
+		return 0;
+	}
+	if (len == 1){
+		return nums[0];
+	}
+	if (len == 2){
+		return nums[0] > nums[1] ? nums[0] : nums[1];
+	}
+	if (nums[0] >= nums[1]){
+		mp[1] = true;
+		dp[0] = nums[0];
+		dp[1] = nums[0];
+	}
+	else{
+		mp[1] = false;
+		dp[0] = nums[0];
+		dp[1] = nums[1];
+	}
+	
+	int i = 2;
+	for (; i < len - 1; ++i){
+
+		if (dp[i - 1] == dp[i - 2] + nums[i]){
+			dp[i] = dp[i - 1];
+			mp[i] = false;
+		}
+		else if (dp[i - 1] > dp[i - 2] + nums[i]){
+			dp[i] = dp[i - 1];
+			if (mp[i - 1] == true){
+				mp[i] = true;
+			}
+			else{
+				mp[i] = false;
 			}
 		}
 		else{
-			//对应位为0
-			if (b[i] == '0'){
-				++correctNum;
+			dp[i] = dp[i - 2] + nums[i];
+			if (mp[i - 2] == true){
+				mp[i] = true;
+			}
+			else{
+				mp[i] = false;
 			}
 		}
 	}
-	double ret = correctNum * 100.0 / totalNum;
-	printf("%.2lf\n", ret);
+	if (dp[i - 1] > dp[i - 2] + nums[i]){
+		dp[i] = dp[i - 1];
+	}
+	else{
+		if (mp[i - 2] == false){
+			dp[i] = dp[i - 2] + nums[i];
+		}
+		else{
+			dp[i] = max(dp[i - 1], dp[i - 2]);
+		}
+	}
+	return dp[i];
+}
+
+int main(){
+	vector<int> v = { 1, 1, 3, 6, 7, 10, 7, 1, 8, 5, 9, 1, 4, 4, 3};
+	int ret = rob(v);
 	return 0;
 }
