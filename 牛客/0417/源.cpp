86993cdac893019913c4423d2bc97ad227d2cd65
Mrzhailiming <401875751@qq.com>
2020-04-18 14:08:04 +0800

    .

---
+++ 牛客/0417/源.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+void PrintNum(int n){
+	double ret = 0;
+	ret = 
+	printf("%.2f%%", ret);
+}
+
+int main(){
+	int n;
+	while (cin >> n){
+		PrintNum(n);
+	}
+	
+	return 0;
+}
